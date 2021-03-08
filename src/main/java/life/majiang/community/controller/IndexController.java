745085869c2bcc0981a9fe13a647584c8011f895
makingitpossible <961599760@qq.com>
2021-03-08 20:38:30 +0800

    added avatar field to user table

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -15,20 +15,23 @@ import javax.servlet.http.HttpServletRequest;
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request) {
         //第一次访问主页时，查询请求域的cookie中是否已有token，来验证是否已有登录信息
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie :
-                cookies) {
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();  //如果有，取得token的值
-                User user = userMapper.findByToken(token);  //已token为条件从数据库中查找user
-                if (user != null) {
-                    //查找结果不为null，就在服务器session域中加入该user对象
-                    request.getSession().setAttribute("user",user);
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie :
+                    cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();  //如果有，取得token的值
+                    User user = userMapper.findByToken(token);  //已token为条件从数据库中查找user
+                    if (user != null) {
+                        //查找结果不为null，就在服务器session域中加入该user对象
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
         }
         //如果cookie中没有token，或者数据库中没有该token对应的user对象，就返回初始页面
